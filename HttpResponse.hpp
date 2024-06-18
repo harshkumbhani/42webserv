c9a51f766534a5950c64e0051d1803e80add2bfd
Oreste <tuoreste@gmail.com>
2024-06-18 19:13:51 +0200

    Feat: Let's commence the http response

---
+++ HttpResponse.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpResponse.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otuyishi <otuyishi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 19:09:00 by otuyishi          #+#    #+#             */
+/*   Updated: 2024/06/18 19:10:37 by otuyishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HTTPRESPONSE_HPP
+# define HTTPRESPONSE_HPP
+
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <string>
+#include <vector>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+class HttpRequest {
+	public:
+
+	private:
+};
+
+#endif
