7d7cbcccd0933a2b98c5c052eb92c6281b3cf025
Oreste <tuoreste@gmail.com>
2024-08-26 15:28:29 +0200

    Add: reducing response error handling redundance problem

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpResponse.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkumbhan <hkumbhan@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: otuyishi <otuyishi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2024/08/26 12:18:32 by hkumbhan         ###   ########.fr       */
+/*   Updated: 2024/08/26 15:27:31 by otuyishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -419,17 +419,12 @@ void HttpResponse::parseRequestBody(clientState &clientData) {
 	std::string fileContent;
 
 		parse_headers(contentStream, fileName, fileContent);
-		// if (fileName.empty())
-		// 	fileName = "unknown";
-		DEBUG("FILE SIZE: " << fileContent.size() << "\n");
 		std::string filePath = "./www/upload/" + fileName;
 		write_to_file(clientData, filePath, fileContent);
 		clientData.fileName = fileName;
 		clientData.bodyString.erase(0, nextBoundaryStart);
 		clientData.flagBodyRead = true;
 		std::cout << "File saved to: " << filePath << std::endl;
-		// std::cout << "File content:\n" << fileContent << std::endl;
-	// }
 	return;
 }
 
@@ -445,14 +440,13 @@ std::string HttpResponse::response_Post(clientState &clientData) {
 	parseRequestBody(clientData);
 	clientData.bodyString.clear();
 	if (clientData.flagFileStatus == true)
-		return (generateHttpResponse(400, "No file was uploaded; please attach a file."));
+		return (generateHttpResponse(400, httpErrorMap.at(400)));
 	
-	return generateHttpResponse(201, "File Created");
+	return generateHttpResponse(201, httpErrorMap.at(201));
 }
 
 
 //================================DELETE=====================================
-
 std::string HttpResponse::generateErrorPage(int code, const std::string& message) {
     return R"(
 			<!DOCTYPE html>
@@ -556,7 +550,7 @@ std::string urlDecode(const std::string& value) {
 				decoded += value[i];
 			}
 		}
-    return decoded;
+	return decoded;
 }
 
 std::string HttpResponse::responseDelete(clientState &clientData) {
@@ -601,11 +595,11 @@ std::string HttpResponse::respond(clientState &clientData) {
 		return respond_Get(clientData);
 	} else if (clientData.requestLine[0] == "POST") {
 		if (clientData.flagFileSizeTooBig)
-			return (generateHttpResponse(413, "Request body size exceeds the maximum allowed size."));
+			return (generateHttpResponse(413, httpErrorMap.at(413)));
 		return response_Post(clientData);
 	} else if (clientData.requestLine[0] == "DELETE") {
 		return responseDelete(clientData);
 	} else {
-		return errorHandlingPost(405, clientData);
+		return generateHttpResponse(405, httpErrorMap.at(405));
 	}
 }
