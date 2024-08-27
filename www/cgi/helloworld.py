1a4a6b72896cbe9966b57a553b7f2f696aac1b67
moheddine-belhaj <moheddinebelhaj@gmail.com>
2024-08-27 13:28:20 +0200

    add helloworld.py

--- www/cgi/helloworld.py
+++ www/cgi/helloworld.py
@@ -1,2 +1,14 @@
-for i in range(10):
-	print("<p>Hello, World!</p>")
+#!/usr/bin/env python3
+
+import cgi
+import cgitb
+cgitb.enable()
+
+form = cgi.FieldStorage()
+param = form.getvalue("param")
+num = int(form.getvalue("num"))
+
+print("Content-Type: text/html")
+print()
+for i in range(num):
+    print(f"<p>{param}</p>")
